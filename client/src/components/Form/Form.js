c799aef9d821ef1e3d10c4e92d4588c0fbaebfe7
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-11 00:26:28 +0200

    Adding the form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Paper, Typography, TextField, Button } from '@material-ui/core'
+import FileBase from 'react-file-base64'
 import useStyles from './styles'
 
 const Form = () => {
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
     const classes = useStyles()
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+    }
+
+    const clear = () => {
+
+    }
+
     return (
-        <div>
-            <h1>Form</h1>
-        </div>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant="h6">Creating a Memory</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(event) => setPostData({ ...postData, creator: event.target.value })} />
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(event) => setPostData({ ...postData, title: event.target.value })} />
+                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(event) => setPostData({ ...postData, message: event.target.value })} />
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(event) => setPostData({ ...postData, tags: event.target.value })} />
+                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}></FileBase></div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+            </form>
+        </Paper>
     )
 }
 
