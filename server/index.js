c55f1e09823749692bd9bb0d9839f343ad63e039
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-11 17:44:44 +0200

    Adding production build to the backend

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ dotenv.config()
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+app.use(express.static('build'))
 
 app.use('/posts', postRoutes)
 
