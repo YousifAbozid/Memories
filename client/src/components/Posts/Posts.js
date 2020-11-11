e963a4d67d4604e856d1b5e175d8f42bf70b461e
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-11 10:56:28 +0200

    Adding the post card to the frontend

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Grid, CircularProgress } from '@material-ui/core'
 import Post from './Post/Post'
 import useStyles from './styles'
 
@@ -10,10 +11,15 @@ const Posts = () => {
     console.log(posts)
 
     return (
-        <div>
-            <h1>Posts</h1>
-            <Post />
-        </div>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
